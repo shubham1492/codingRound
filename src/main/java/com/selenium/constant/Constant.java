186391e9e8d53335b0deea121b1578735e763639
“Shubham” <shubhamjain.acts@gmail.com>
2019-08-20 01:26:01 +0530

    Added constant for Source code.

---
+++ src/main/java/com/selenium/constant/Constant.java
@@ -0,0 +1,20 @@
+package com.selenium.constant;
+
+public class Constant {
+
+      /**************** Constant For Browser Type *************************/
+	
+	public static final String IEXPLORE= "iexplore";
+	public static final String FIREFOX= "firefox";
+	public static final String CHROME= "chrome";
+	
+	
+	/**************** Timeout type****************/
+	
+	public static final int SHORT_TIMEOUT=3000;
+	public static final int MEDIUM_TIMEOUT=5000;
+	public static final int LONG_TIMEOUT=7000;
+
+	public static final String TARGET_URL="https://www.cleartrip.com/";
+	
+}
